1d4d26ea2cfdb547b711fd9e864d15c30e6c2b79
Truc Nguyen <trucnguyen_2025@depauw.edu>
2022-09-24 13:37:46 -0400

    changes for front-end

--- app/src/main/java/com/example/robinhood/MainActivity.kt
+++ app/src/main/java/com/example/robinhood/MainActivity.kt
@@ -2,10 +2,11 @@ package com.example.robinhood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
     }
 }
