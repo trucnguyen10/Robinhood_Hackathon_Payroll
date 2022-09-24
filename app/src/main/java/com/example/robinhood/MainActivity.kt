63de824a6e17b87f71c135f7fe7640997bb0c220
Mahlangu Nzunda <nzundamahlangu@gmail.com>
2022-09-24 14:12:36 -0500

    implement employee data model and recyclerview adapter

--- app/src/main/java/com/example/robinhood/MainActivity.kt
+++ app/src/main/java/com/example/robinhood/MainActivity.kt
@@ -3,10 +3,26 @@ package com.example.robinhood
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.robinhood.Models.EmployeeDataSource
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var employeeAdapter: EmployeeAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register);
+        setContentView(R.layout.activity_main);
+
+        initializeRecyclerView();
+    }
+
+    private fun initializeRecyclerView() {
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        employeeAdapter = EmployeeAdapter(this)
+        recyclerView.adapter = employeeAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        employeeAdapter.setData(EmployeeDataSource.createEmployeeDate())
     }
 }
---
+++ app/src/main/java/com/example/robinhood/Models/Employee.kt
@@ -0,0 +1,9 @@
+package com.example.robinhood.Models
+
+data class Employee(
+    val name: String,
+    val role : String,
+    val salary : Int,
+    val profilePics : Int,
+    val accountAddress : String,
+)
